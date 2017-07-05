e8f871da090c041acec76415398b66e88d03b0d1
yichenghuifu--IOS <zyy@poszd.com>
2017-07-05 11:53:59 +0800

    Initial Commit

---
+++ ZYYRandCodeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYYRandCodeDemo
+//
+//  Created by liguo.chen on 17/7/5.
+//  Copyright © 2017年 Slience. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYYRandCodeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYYRandCodeDemo
+//
+//  Created by liguo.chen on 17/7/5.
+//  Copyright © 2017年 Slience. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
